699660db740fce38513cdaf9a0c32a11524c8dc3
eskim <etrilles@my.centennialcollege.ca>
2025-11-29 17:09:57 -0500

    Ready for deployment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ import morgan from "morgan";
 import mongoose from "mongoose";
 import dotenv from "dotenv/config";
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 mongoose.connect(process.env.MONGODB_URI);
 const connection = mongoose.connection;
 connection.on('error', console.error.bind(console, "MongoDB connection error: "));
@@ -18,11 +24,17 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.get('/api', (req, res) => { res.status(201).json({ message: "Welcome to My Portfolio application." });});
+app.get('/api', (req, res) => { res.status(201).json({ message: "Welcome to My Portfolio application." }); });
 app.use('/api/projects', projectRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/contacts', contactRoutes);
 app.use('/api/qualifications', qualificationRoutes);
+
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(pathToFileURL.join(__dirname, '../client/dist', 'index.html'));
+});
 
 app.listen(3000);
 
